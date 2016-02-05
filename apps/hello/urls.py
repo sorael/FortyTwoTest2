913e81339fd1fcb0d59ed752813eba7c135d473a
Anatolii Soroka <sorokaanatolii@gmail.com>
2016-02-05 13:42:45 +0200

    ticket:3 update urls.py, view, created template for requests with hard-coded data

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     '',
     url(r'^$', 'apps.hello.views.view_contact', name='index'),
+    url(r'^requests/', 'apps.hello.views.view_requests', name='requests'),
 )
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -6,3 +6,7 @@ from apps.hello.models import Person
 def view_contact(request):
     contact = Person.objects.first()
     return render(request, 'hello/index.html', {'contact': contact})
+
+
+def view_requests(request):
+    return render(request, 'hello/requests.html')
