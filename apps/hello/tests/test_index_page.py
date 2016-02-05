a3db1dc6f8d565e83bed444d2baf82d308e89a39
Anatolii Soroka <sorokaanatolii@gmail.com>
2016-02-05 11:54:20 +0200

    ticket:1 created test for model, storing the data for the view

--- apps/hello/tests/test_index_page.py
+++ apps/hello/tests/test_index_page.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from apps.hello.models import Person
 
 
 class IndexPageTests(TestCase):
@@ -22,3 +23,20 @@ class IndexPageTests(TestCase):
         self.assertContains(
             response,
             '<h1 class="text-center">42 Coffee Cups Test Assignment</h1>')
+
+    def test_data_index(self):
+        """
+        Is data from db is show on the index page
+        """
+        response = self.client.get(reverse('index'))
+        contacts = Person.objects.all()
+        self.assertEqual(contacts.count(), 1)
+        contact = Person.objects.first()
+        self.assertContains(response, contact.first_name, 1)
+        self.assertContains(response, contact.last_name, 1)
+        self.assertContains(response, contact.bio, 1)
+        self.assertContains(response, contact.email, 1)
+        self.assertContains(response, contact.jabber, 1)
+        self.assertContains(response, contact.skype, 1)
+        self.assertContains(response, contact.other, 1)
+        self.assertTrue(contact.id == 1)
