fa955f3299f98d64bff06e6b1ba37693a4ef2b42
Anatolii Soroka <sorokaanatolii@gmail.com>
2016-02-05 14:23:33 +0200

    ticket:3 created the view, returning hard-coded data for the template

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -9,4 +9,10 @@ def view_contact(request):
 
 
 def view_requests(request):
-    return render(request, 'hello/requests.html')
+    requests = [{'date_time': '2016-02-05 10:02:43',
+                 'method': 'GET',
+                 'file_path': '/',
+                 'ver_protocol': 'HTTP/1.1',
+                 'status': '200',
+                 'content': '1508'}]
+    return render(request, 'hello/requests.html', {'requests': requests * 10})
