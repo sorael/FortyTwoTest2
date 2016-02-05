1fdc282d46ece75ecd8c4d8ae28185c7c0655be8
Anatolii Soroka <sorokaanatolii@gmail.com>
2016-02-05 11:48:13 +0200

    ticket:1 created view, returning hard-coded data

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 
 
 def view_contact(request):
-    return render(request, 'hello/index.html')
+    contact = {
+        'first_name': 'Anatolii',
+        'last_name': 'Soroka',
+        'date_of_birth': '1981.02.21',
+        'bio': 'Junior Python/Django',
+        'email': 'sorokaanatolii@gmail.com',
+        'jabber': 'a-soroka@khavr.com',
+        'skype': 's-sorael',
+        'other': 'Mobile phone: +380684021358'
+    }
+    return render(request, 'hello/index.html', {'contact': contact})
