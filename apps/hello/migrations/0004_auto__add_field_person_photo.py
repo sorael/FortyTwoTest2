14def2917365ea9dfd0b6220ea532eeb17285ee3
Anatolii Soroka <sorokaanatolii@gmail.com>
2016-02-08 12:22:05 +0200

    ticket:5 updated urls.py (added MEDIA_ROOT)

---
+++ apps/hello/migrations/0004_auto__add_field_person_photo.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Person.photo'
+        db.add_column(u'hello_person', 'photo',
+                      self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Person.photo'
+        db.delete_column(u'hello_person', 'photo')
+
+
+    models = {
+        u'hello.person': {
+            'Meta': {'object_name': 'Person'},
+            'bio': ('django.db.models.fields.TextField', [], {}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'other': ('django.db.models.fields.TextField', [], {}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'hello.request': {
+            'Meta': {'ordering': "['-date_time']", 'object_name': 'Request'},
+            'content': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'date_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'file_path': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'method': ('django.db.models.fields.CharField', [], {'max_length': '7'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
+            'ver_protocol': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        }
+    }
+
+    complete_apps = ['hello']
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns(
@@ -9,3 +11,4 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
 )
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
