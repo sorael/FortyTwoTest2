1ad94bf60eaf19577bd20b619969eb5b667d7e18
Anatolii Soroka <sorokaanatolii@gmail.com>
2016-02-11 16:17:38 +0200

    ticket:13 updated Request model (add priority field)

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -23,12 +23,16 @@ class Person(models.Model):
 
 
 class Request(models.Model):
+
+    CHOICES = ((i, i) for i in range(1, 4))
+
     date_time = models.DateTimeField(auto_now_add=True)
     method = models.CharField(max_length=7)
     file_path = models.CharField(max_length=250)
     ver_protocol = models.CharField(max_length=10)
     status = models.CharField(max_length=3)
     content = models.CharField(max_length=8)
+    priority = models.IntegerField(default=1, choices=CHOICES)
 
     def as_dict(self):
         return {'date_time': str(self.date_time)[:19],
@@ -37,6 +41,7 @@ class Request(models.Model):
                 'ver_protocol': self.ver_protocol,
                 'status': self.status,
                 'content': self.content,
+                'priority': self.priority,
                 'id': self.id
                 }
 
