e9edecbe0afb92cfe0ab534f70a2b91ead270ab4
Anatolii Soroka <sorokaanatolii@gmail.com>
2016-02-12 11:31:03 +0200

    ticket:13 fixed RequestMiddleware (excluded '/requests_count/' and '/priority_requests/' urls from logging Request object)

--- apps/hello/middleware.py
+++ apps/hello/middleware.py
@@ -5,10 +5,11 @@ from apps.hello.models import Request
 class RequestsMiddleware(object):
 
     def process_response(self, request, response):
-        Request.objects.create(
-            method=request.META['REQUEST_METHOD'],
-            file_path=request.path,
-            ver_protocol=request.META['SERVER_PROTOCOL'],
-            status=response.status_code,
-            content=len(response.content))
+        if request.path not in ['/priority_requests/', '/requests_count/']:
+            Request.objects.create(
+                method=request.META['REQUEST_METHOD'],
+                file_path=request.path,
+                ver_protocol=request.META['SERVER_PROTOCOL'],
+                status=response.status_code,
+                content=len(response.content))
         return response
